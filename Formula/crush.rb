4c8d72ba791bb48768e19dc94059441f53c90d13
OrkWard <ork.ward@icloud.com>
2026-03-04 02:03:04 +0800

    crush: update to v0.47.0

--- Formula/crush.rb
+++ Formula/crush.rb
@@ -1,13 +1,13 @@
 class Crush < Formula
   desc "A powerful AI assistant that runs in the CLI"
   homepage "https://github.com/OrkWard/crush"
-  version "0.46.3"
+  version "0.47.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/OrkWard/crush/releases/download/v0.46.3/crush_Darwin_arm64.tar.gz"
-      sha256 "be387f27ace59eea4d780a88563f55f79fbbb2655530e2bd0be1113601e68046"
+      url "https://github.com/OrkWard/crush/releases/download/v0.47.0/crush_Darwin_arm64.tar.gz"
+      sha256 "01c738da22f5f6465b0f1207f22688674d74d1b4e8446ea919d1c24eb1fa4b5e"
     end
   end
 
