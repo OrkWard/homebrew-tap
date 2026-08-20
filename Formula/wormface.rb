181978815140295fbd0926c0a6ec2c0a2467e512
OrkWard <ork.ward@icloud.com>
2026-08-20 23:15:56 +0800

    wormface: add 2.0.0 bottle.

--- Formula/wormface.rb
+++ Formula/wormface.rb
@@ -6,6 +6,12 @@ class Wormface < Formula
   license "MIT"
   head "https://github.com/orkward/wormface.git", using: :git, branch: "master"
 
+  bottle do
+    root_url "https://github.com/orkward/wormface/releases/download/v2.0.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce8bd9ba1fca2fccf011e7ad1276d749c8fed9a58e67cabab56fb05511b7a7d9"
+  end
+
   depends_on "go" => :build
 
   def install
